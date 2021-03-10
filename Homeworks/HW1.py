c8688fc2c2d0a3af2804660c825a0900d4aac3a1
sbayramoglu26 <80282349+sbayramoglu26@users.noreply.github.com>
2021-03-10 22:05:20 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,15 @@
-#Explain your work
+odd_numbers = [11, 13, 15, 17, 19]
+even_numbers = [12, 14, 16, 18, 20]
+print("Odd Numbers List: ", odd_numbers)
+print("Even Numbers List: ", even_numbers)
 
-#Question 1
-for x in range(a):
-	print(a)
+odd_numbers.extend(even_numbers)
+merged_list = odd_numbers
+
+print("Merged List: ", merged_list)
+
+new_mergedlist = [i * 2 for i in merged_list]
+print(new_mergedlist)
+for i in new_mergedlist:
+  print("Data Type of {} value:".format(i))
+  print(type(i))

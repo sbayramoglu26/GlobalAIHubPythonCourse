6646b07cb9048550e0998f68ce9e906d9d7fe24c
sbayramoglu26 <80282349+sbayramoglu26@users.noreply.github.com>
2021-03-11 18:25:20 +0300

    Update HW2.py

--- Homeworks/HW2.py
+++ Homeworks/HW2.py
@@ -1,5 +1,45 @@
-#Explain your work
-
-#Question 1
-for x in range(a):
-	print(a)
+cv_1={"Name":"Ahmet",
+      "Surname":"Demir",
+      "Nationality":"Turkish",
+      "Birth of Date (DD/MM/YYYY)":"29.12.1990",
+      "Graduated From":"Ankara University",
+      "Job Title":"Computer Engineer"}
+cv_2={"Name":"Burak",
+      "Surname":"Gürel",
+      "Nationality":"Turkish",
+      "Birth of Date (DD/MM/YYYY)":"12.6.1994",
+      "Graduated From":"University of Health Sciences",
+      "Job Title":"Occupational Health and Safety"}
+cv_3={"Name":"Kadir",
+      "Surname":"Avcı",
+      "Nationality":"Turkish",
+      "Birth of Date (DD/MM/YYYY)":"19.3.1982",
+      "Graduated From":"Düzce University",
+      "Job Title":"Backend Developer"}
+cv_4={"Name":"Vladimir",
+      "Surname":"Morozova",
+      "Nationality":"Russian",
+      "Birth of Date (DD/MM/YYYY)":"28.2.1995",
+      "Graduated From":"Kuban State University of Technology",
+      "Job Title":"3D Artist"}
+cv_5={"Name":"Bogdan",
+      "Surname":"Jakovlevich",
+      "Nationality":"Polish",
+      "Birth of Date (DD/MM/YYYY)":"05.6.1993",
+      "Graduated From":"Warsaw University of Technology",
+      "Job Title":"Game Designer"}
+print("CV 1:")
+for i,j in cv_1.items():
+    print(i,':',j)
+print("\nCV 2:")
+for k,v in cv_2.items():
+    print(k, ":",v)
+print("\nCV 3:")
+for a,b in cv_3.items():
+    print(a, ":",b)
+print("\nCV 4:")
+for c,d in cv_4.items():
+    print(c,":",d)
+print("\nCV 5:")
+for e,f in cv_5.items():
+    print(e,":",f)
